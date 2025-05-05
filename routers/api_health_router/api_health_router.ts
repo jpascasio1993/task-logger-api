44e41d0c46199400c84a840676c6726748ff1865
Jomar <jpascasio1993@gmail.com>
2025-05-05 12:04:01 +0800

    update

--- routers/api_health_router/api_health_router.ts
+++ routers/api_health_router/api_health_router.ts
@@ -2,6 +2,9 @@ import { Router } from "express";
 
 const router  = Router();
 
+// this is used to check if the server is running
+// and also is used by the app to check if the device 
+// has internet connection
 router.get('/health', (req, res) => {
     res.status(200).json(true);
 });
--- routers/middleware/middleware.ts
+++ routers/middleware/middleware.ts
@@ -10,11 +10,6 @@ export const inject = (
   next();
 };
 
-// export const errorHandler: ErrorRequestHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-//     console.error(err);
-//     res.status(500).json({ message: 'Internal Server Error' });
-// }
-
 export default {
   inject,
 };
